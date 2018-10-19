93efdfe2aec55965a6d0b88b3e4d5894cf7d3f2c
Jacky Wijaya <jekiwijaya@hotmail.com>
2018-10-19 15:02:45 +0700

    fix(required): validation for empty object

--- packages/validation/src/required.ts
+++ packages/validation/src/required.ts
@@ -1,7 +1,13 @@
 import { ValidationResult, Validation } from "./types";
 
 export const required: Validation = (value: any, errorMessage?: string): ValidationResult => {
-  if (value === null || value === undefined || value === '' || (typeof value === 'object' && Object.keys(value).length === 0))
+  if (
+    value === null ||
+    value === undefined ||
+    value === '' ||
+    (Object.keys(value).length === 0 && value.constructor === Object) ||
+    (Array.isArray(value) && !value.length)
+  )
     return errorMessage || 'Required';
   return null;
 }
